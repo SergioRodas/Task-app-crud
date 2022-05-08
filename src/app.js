1993e0e1ef9d9f7cff9765d3e16d132911c4b474
sergiorodas <sergiorodasgonzalez@gmail.com>
2022-05-07 23:42:41 -0300

    Agrega handlebars

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import { create } from 'express-handlebars';
+import routes from './routes/index.routes';
+import path from 'path';
 
 const app = express();
-import routes from './routes/index.routes';
+
+app.set('views', path.join(__dirname, 'views'));
+
+const exphbs = create({
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    defaultLayout: 'main',
+    extname: '.hbs',
+});
+
+app.engine('.hbs', exphbs.engine);
+app.set('view engine', '.hbs');
 
 import './database';
 
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,15 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('hello world');
+    res.render('index');
+})
+
+router.get('/about', (req, res) => {
+    res.render('about');
+})
+
+router.get('/edit', (req, res) => {
+    res.render('edit');
 })
 
 module.exports = router;
